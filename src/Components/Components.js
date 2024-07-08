7c3c611343282207a8f23c7bee8772c7f4e4c93d
RuyuanWan <30328069+RuyuanWan@users.noreply.github.com>
2024-07-07 23:26:42 -0400

    Update Components.js

--- src/Components/Components.js
+++ src/Components/Components.js
@@ -4,6 +4,10 @@ import Main from "./Main/Main";
 import Comments from "./Comments/Comments";
 import Footer from "./Footer/Footer";
 
+import AuthModule from "./Auth/Auth.js";
+import AuthRegister from "./Auth/AuthRegister";
+import AuthLogin from "./Auth/AuthLogin";
+
 const Components = () => {
   return (
     <Router>
@@ -16,11 +20,17 @@ const Components = () => {
             <li>
               <Link to="/comments">Comments</Link> {/* Link to navigate to the Comments component */}
             </li>
+            <li>
+              <Link to="/auth">Test Auth</Link> {/* Link to navigate to the TestAuth component */}
+            </li>
           </ul>
         </nav>
         <Routes>
           <Route path="/" element={<Main />} /> {/* Route for the Main component */}
           <Route path="/comments" element={<Comments />} /> {/* Route for the Comments component */}
+          <Route path="/auth" element={<AuthModule />} /> {/* Route for the Auth component */}
+          <Route path="/auth/register" element={<AuthRegister />} /> {/* Route for the AuthRegister component */}
+          <Route path="/auth/login" element={<AuthLogin />} /> {/* Route for the AuthLogin component */}
         </Routes>
         <Footer /> {/* Footer component, displayed on all pages */}
       </div>
